d73bc60f443786c439dade4c75bdf2974de932d7
VinvinVinayakvinVin <vinayakshuklakalapnat@live.nl>
2021-01-26 20:31:44 +0100

    training mode has now buttons with mathematic topics

--- app/src/main/java/com/example/android/mathematicstrainingmode/TrainingModeBeginScreen.java
+++ app/src/main/java/com/example/android/mathematicstrainingmode/TrainingModeBeginScreen.java
@@ -1,5 +1,6 @@
 package com.example.android.mathematicstrainingmode;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,17 +11,48 @@ public class TrainingModeBeginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training_mode_begin_screen);
-
-        buttonIntents();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(getApplicationContext().getResources().getColor(R.color.dark_bg));
+        }
     }
 
-    private void buttonIntents() {
-//        findViewById(R.id.single_game_btn).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(HomeScreenActivity.this, SingleGameActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+    public void button1() {
+
+    }
+
+    public void button2() {
+
+    }
+
+    public void button3() {
+
+    }
+
+    public void button4() {
+
+    }
+
+    public void button5() {
+
+    }
+
+    public void button6() {
+
+    }
+
+    public void button7() {
+
+    }
+
+    public void button8() {
+
+    }
+
+    public void button9() {
+
+    }
+
+    public void button10() {
+
     }
 }
