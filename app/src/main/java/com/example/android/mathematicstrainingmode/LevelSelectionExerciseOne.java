dbc27a07004c1d63d96fb04b06df092ff9dcd7e0
VinvinVinayakvinVin <vinayakshuklakalapnat@live.nl>
2021-01-28 21:20:48 +0100

    working buttons w.i.p

--- app/src/main/java/com/example/android/mathematicstrainingmode/LevelSelectionExerciseOne.java
+++ app/src/main/java/com/example/android/mathematicstrainingmode/LevelSelectionExerciseOne.java
@@ -1,9 +1,12 @@
 package com.example.android.mathematicstrainingmode;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -17,8 +20,10 @@ public class LevelSelectionExerciseOne extends AppCompatActivity {
     private LinearLayout _linearLayout;
     private LinearLayout _rowOfLevels[] = new LinearLayout[34];
     private Button _buttonLevel[] = new Button[100];
-    private LinearLayout.LayoutParams _buttonParams;
+    private LinearLayout.LayoutParams[] _buttonParams = new LinearLayout.LayoutParams[4];
     private ImageView _backButton;
+
+    private boolean[] levelSucceeded = new boolean[100];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,38 +32,15 @@ public class LevelSelectionExerciseOne extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setNavigationBarColor(getApplicationContext().getResources().getColor(R.color.dark_bg));
         }
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences("SharedPrefKey", Context.MODE_PRIVATE);
 
         _linearLayout = findViewById(R.id.level_selection_exercise_one_contents);
         _linearLayout.setWeightSum(34);
 
-        /** TODO fix the margins of the buttons, and remove the old code
+        /** TODO make working buttons...
          *  TODO also make it for the other 9 exercises
-         *  TODO when you select the level, you go to the next activity and half of the activity screen is the question and in the middle is the time you got to answer the question. */
-
-//        LinearLayout rowtest = new LinearLayout(this);
-//        rowtest.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-//        rowtest.setOrientation(LinearLayout.HORIZONTAL);
-//        rowtest.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.neon_blue)); // setbackground is a test, don't use it for full release in this code.
-//        rowtest.setWeightSum(3);
-//        _linearLayout.addView(rowtest);
-//
-//        Button but1 = new Button(this);
-//        Button but2 = new Button(this);
-//        Button but3 = new Button(this);
-//        but1.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1));
-//        but2.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1));
-//        but3.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1));
-//        but1.setText("1");
-//        but2.setText("2");
-//        but3.setText("3");
-//        but1.setTextSize(25);
-//        but2.setTextSize(25);
-//        but2.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.neon_orange));
-//        but3.setTextSize(25);
-//
-//        rowtest.addView(but1);
-//        rowtest.addView(but2);
-//        rowtest.addView(but3);
+         *  TODO when you select the level, you go to the next activity
+         *  TODO and half of the activity screen is the question and in the middle is the time you got to answer the question. */
 
         for (int i = 0; i < 33; i++) {
             System.out.println("level: " + (i + 1));
@@ -73,27 +55,58 @@ public class LevelSelectionExerciseOne extends AppCompatActivity {
             _buttonLevel[(i * 3) + 1] = new Button(this);
             _buttonLevel[(i * 3) + 2] = new Button(this);
 
-            _buttonParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1);
-            _buttonParams.setMargins(30, 10, 30, 10);
+            _buttonParams[0] = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1);
+            _buttonParams[1] = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1);
+            _buttonParams[2] = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1);
+            _buttonParams[3] = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1);
 
-            _buttonLevel[(i * 3)].setLayoutParams(_buttonParams);
-            _buttonLevel[(i * 3) + 1].setLayoutParams(_buttonParams);
-            _buttonLevel[(i * 3) + 2].setLayoutParams(_buttonParams);
+            if (i == 0) {
+                _buttonParams[0].setMargins(60, 40, 30, 20);
+                _buttonParams[1].setMargins(30, 40, 30, 20);
+                _buttonParams[2].setMargins(30, 40, 60, 20);
+            } else {
+                _buttonParams[0].setMargins(60, 20, 30, 10);
+                _buttonParams[1].setMargins(30, 20, 30, 10);
+                _buttonParams[2].setMargins(30, 20, 60, 10);
+            }
 
-            _buttonLevel[(i * 3)].setText("" + ((i * 3)));
-            _buttonLevel[(i * 3) + 1].setText("" + ((i * 3) + 1));
-            _buttonLevel[(i * 3) + 2].setText("" + ((i * 3) + 2));
+            _buttonLevel[(i * 3)].setLayoutParams(_buttonParams[0]);
+            _buttonLevel[(i * 3) + 1].setLayoutParams(_buttonParams[1]);
+            _buttonLevel[(i * 3) + 2].setLayoutParams(_buttonParams[2]);
+
+            _buttonLevel[(i * 3)].setText("" + ((i * 3) + 1));
+            _buttonLevel[(i * 3) + 1].setText("" + ((i * 3) + 2));
+            _buttonLevel[(i * 3) + 2].setText("" + ((i * 3) + 3));
             _buttonLevel[(i * 3)].setTextSize(25);
             _buttonLevel[(i * 3) + 1].setTextSize(25);
             _buttonLevel[(i * 3) + 2].setTextSize(25);
 
-            _buttonLevel[(i * 3)].setBackgroundColor(getApplicationContext().getResources().getColor(R.color.neon_orange));
-            _buttonLevel[(i * 3) + 1].setBackgroundColor(getApplicationContext().getResources().getColor(R.color.neon_orange));
-            _buttonLevel[(i * 3) + 2].setBackgroundColor(getApplicationContext().getResources().getColor(R.color.neon_orange));
+                _buttonLevel[(i * 3)].setBackgroundColor(getApplicationContext().getResources().getColor(R.color.locked_button));
+                _buttonLevel[(i * 3) + 1].setBackgroundColor(getApplicationContext().getResources().getColor(R.color.neon_orange));
+                _buttonLevel[(i * 3) + 2].setBackgroundColor(getApplicationContext().getResources().getColor(R.color.locked_button));
 
             _rowOfLevels[i].addView(_buttonLevel[(i * 3)]);
             _rowOfLevels[i].addView(_buttonLevel[(i * 3) + 1]);
             _rowOfLevels[i].addView(_buttonLevel[(i * 3) + 2]);
+
+            _buttonLevel[(i * 3)].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+            _buttonLevel[(i * 3) + 1].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+            _buttonLevel[(i * 3) + 2].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
         }
         System.out.println("level: " + 34);
         _rowOfLevels[33] = new LinearLayout(this);
@@ -102,14 +115,20 @@ public class LevelSelectionExerciseOne extends AppCompatActivity {
         _linearLayout.addView(_rowOfLevels[33]);
 
         _buttonLevel[99] = new Button(this);
-        _buttonLevel[99].setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1));
+        _buttonParams[3].setMargins(60, 20, 60, 40);
+        _buttonLevel[99].setLayoutParams(_buttonParams[3]);
         _buttonLevel[99].setText("100");
         _buttonLevel[99].setTextSize(25);
         _buttonLevel[99].setBackgroundColor(getApplicationContext().getResources().getColor(R.color.neon_orange));
         _rowOfLevels[33].addView(_buttonLevel[99]);
+        _buttonLevel[99].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
 
-
+        // back button
         _backButton = findViewById(R.id.level_selection_exercise_one_back_button);
         _backButton.setOnClickListener(new View.OnClickListener() {
             @Override
