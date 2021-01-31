f4120c4eded491b2d06e9f6b3f42767e6c994ec6
VinvinVinayakvinVin <vinayakshuklakalapnat@live.nl>
2021-01-31 23:53:16 +0100

    questions for exercise one in the make...

--- app/src/main/java/com/example/android/mathematicstrainingmode/ExerciseOne.java
+++ app/src/main/java/com/example/android/mathematicstrainingmode/ExerciseOne.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Html;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -14,8 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ExerciseOne extends AppCompatActivity {
 
-    // TODO make the questions and set the line in the middle of the screen...
-    // TODO line is made out 60 (seconds) Linear layouts with the gray background inside a parent layout, in this case linear layout...
+    // TODO make the questions...
 
     final int totalSec = 60;
     private LinearLayout[] _sixtyOfALine = new LinearLayout[totalSec];
@@ -30,9 +30,37 @@ public class ExerciseOne extends AppCompatActivity {
 
         buttonIntents();
 
-        int levelnum = getIntent().getIntExtra("levelnum", 1);
+        int levelNum = getIntent().getIntExtra("levelnum", 1);
         TextView actionbarTitle = findViewById(R.id.action_bar_title_exercise_one);
-        actionbarTitle.setText(getApplicationContext().getString(R.string.assigning_numbers_their_places) + ". Level: " + levelnum + " / " + 100);
+        actionbarTitle.setText(getApplicationContext().getString(R.string.assigning_numbers_their_places) + ". Level: " + levelNum + " / " + 100);
+
+        // ------------------- <Make Mathematical Questions! (begins) -----------------
+
+        String question = "Which is larger, ";
+        double num1, num2;
+
+        if (levelNum >= 1 && levelNum < 20) {
+            // fase 1 (natural numbers)
+            num1 = Math.random() + 1;
+            num2 = Math.random() + 1;
+        } else if (levelNum >= 20 && levelNum < 40) {
+            // fase 2 (integers)
+
+        } else if (levelNum >= 40 && levelNum < 60) {
+            // fase 3 (decimals)
+
+        } else if (levelNum >= 60 && levelNum < 80) {
+            // fase 4 (fractions)
+
+        } else if (levelNum >= 80 && levelNum < 100) {
+            // fase 5 (mix of all 4 fases)
+
+        } else if (levelNum == 100) {
+            // fase 6 (final fase - all fases but on max difficulty)
+
+        }
+
+        // ------------------- <Make Mathematical Questions! (ends) -----------------
 
         //line code
         LinearLayout line = findViewById(R.id.exercise_one_line);
@@ -46,7 +74,7 @@ public class ExerciseOne extends AppCompatActivity {
             line.addView(_sixtyOfALine[i]);
         }
 
-        final TextView tvTime = findViewById(R.id.testtv);
+        final TextView tvTime = findViewById(R.id.question);
         final boolean[] isTimerDone = {false};
         final int[] stop = {0};
 
@@ -67,6 +95,17 @@ public class ExerciseOne extends AppCompatActivity {
                     }
                 });
 
+                Button butB = findViewById(R.id.button_B);
+                butB.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startTime = System.currentTimeMillis() - 1000;
+                        for (int i = 0; i < totalSec; i++) {
+                            _sixtyOfALine[i].setVisibility(View.VISIBLE);
+                        }
+                    }
+                });
+
                 while (!isTimerDone[0]) {
                     try {
                         Thread.sleep(1000);
@@ -78,7 +117,7 @@ public class ExerciseOne extends AppCompatActivity {
                         public void run() {
                             if ((int) (totalSec - ((System.currentTimeMillis() - startTime) / 1000)) >= 1) {
                                 _sixtyOfALine[(int) (totalSec - ((System.currentTimeMillis() - startTime) / 1000))].setVisibility(View.INVISIBLE);
-                                tvTime.setText("" + (int) (totalSec - ((System.currentTimeMillis() - startTime) / 1000)));
+//                                tvTime.setText("" + (int) (totalSec - ((System.currentTimeMillis() - startTime) / 1000)));
                             } else {
                                 isTimerDone[0] = true;
                                 if (isTimerDone[0]) {
